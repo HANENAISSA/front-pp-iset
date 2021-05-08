cfb7c910e470ec10e84d43a53eab31bdae75023a
Nawres-cherni <cherninawress22@gmail.com>
2021-05-08 02:43:39 +0100

    demande

--- src/app/teams/club/platform_club/liste-demandes/liste-demandes.component.ts
+++ src/app/teams/club/platform_club/liste-demandes/liste-demandes.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { RequestService } from '../../services/request.service';
 @Component({
   selector: 'app-liste-demandes',
   templateUrl: './liste-demandes.component.html',
   styleUrls: ['./liste-demandes.component.scss']
 })
 export class ListeDemandesComponent implements OnInit {
+  requests: any=[];
 
-  constructor() { }
 
-  ngOnInit() {
+
+
+  constructor(private _http:RequestService,private route: ActivatedRoute)
+  { }
+
+
+  ngOnInit(): void {
+    this.getrequests();
+
+  }
+  getrequests() {
+    this._http.getRequests().subscribe(club => {
+        this.requests= club['data'];
+        console.log(club);
+      },
+      error => {
+        console.log(error);
+      });
+
   }
 
+  Accepter(iddemande:any, email:any ){
+     this._http.acceptRequests(iddemande,email).subscribe(data => {
+      console.log(data)
+      if(data['error']!=true){
+
+        this.getrequests();
+
+      }else{
+        alert(data['message'])
+      }
+
+    },
+      err => {
+    //show error toast when the server went wrong
+      }
+    );
+
+  }
+  Delete(iddemande:any, email:any ){
+    this._http.DeleteRequests(iddemande,email).subscribe(data => {
+     console.log(data)
+     if(data['error']!=true){
+      this.getrequests();
+
+     }else{
+       alert(data['message'])
+     }
+
+   },
+     err => {
+   //show error toast when the server went wrong
+     }
+   );
+
+ }
 }
