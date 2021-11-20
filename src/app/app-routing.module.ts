07dea2014c66de7a9e04b6880e208809bb125a9c
hayet <35689010+hayetbh@users.noreply.github.com>
2021-11-20 23:42:39 +0100

    change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,24 +8,6 @@ import { ProfileComponent } from "./pages/user/profile/profile.component";
 
 
 //club-components
-import { ClubsComponent } from "./teams/club/general/clubs/clubs.component";
-import { EnvoyerDemandeComponent } from "./teams/club/general/envoyer-demande/envoyer-demande.component";
-import { ResetPasswordComponent } from "./teams/club/general/reset-password/reset-password.component";
-import { SigninComponent } from "./teams/club/general/signin/signin.component";
-import { AccueilClubComponent } from "./teams/club/platform_club/accueil-club/accueil-club.component";
-import { CalendrierComponent } from "./teams/club/platform_club/calendrier/calendrier.component";
-
-import { DashboardClubComponent } from "./teams/club/platform_club/dashboard-club/dashboard-club.component";
-import { ListParticipesComponent } from "./teams/club/platform_club/list-participes/list-participes.component";
-import { ListeDemandesComponent } from "./teams/club/platform_club/liste-demandes/liste-demandes.component";
-import { ListeParticipationComponent } from "./teams/club/platform_club/liste-participation/liste-participation.component";
-import { MembresComponent } from "./teams/club/platform_club/membres/membres.component";
-import { ProfileMembreComponent } from "./teams/club/platform_club/profile-membre/profile-membre.component";
-import { ScheduleComponent } from "./teams/club/general/schedule/schedule.component";
-import { TestaccueilComponent } from "./teams/club/platform_club/testaccueil/testaccueil.component";
-import { DetailsEventComponent } from "./teams/club/platform_club/details-event/details-event.component";
-import { DetailEventAccueilComponent } from "./teams/club/general/detail-event-accueil/detail-event-accueil.component";
-import { SendRequestComponent } from "./teams/club/general/send-request/send-request.component";
 
 //administration-components
 //communication-components
@@ -97,64 +79,6 @@ const routes: Routes = [
   },
 
 //club-routes
-{ path: "test", component: TestaccueilComponent },
-{ path: "club/send", component:SendRequestComponent },
-{ path: "club/signin", component: SigninComponent },
-{ path: "club/reset_password", component: ResetPasswordComponent },
-{ path: "accueil/clubs", component: ClubsComponent },
-{ path: "club/calendrier/details/:id", component: DetailEventAccueilComponent },
-{ path: "clubs/envoyer_demande/:id", component: EnvoyerDemandeComponent },
-{ path: "club/calendrier", component: ScheduleComponent},
-{
-  path: "dashboard_club",
-  component: DashboardClubComponent,
-  children: [
-    {
-      path: "accueil/:id",
-      component: AccueilClubComponent,
-    },
-    {
-      path: "calendrier/:id",
-      component: CalendrierComponent,
-    },
-
-    {
-      path: "membres/:id",
-      component: MembresComponent,
-    },
-    {
-      path: "profile/:id",
-      component: ProfileMembreComponent,
-    },
-    {
-      path: "liste-demandes/:id",
-      component: ListeDemandesComponent,
-    },
-    {
-      path: "liste-participes/:id",
-      component: ListParticipesComponent,
-    },
-    {
-      path: "liste-events/:id",
-      component: ListeParticipationComponent,
-    },
-
-    {
-      path: "accueil",
-      component: TestaccueilComponent,
-    },
-
-    {
-      path: "details/:idc/:id",
-      component: DetailsEventComponent,
-    },
-
-    {
-      path: "details/:id",
-      component: DetailsEventComponent,
-    }
-  ],
-},
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,7 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA} from '@angular/core'
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatCardModule} from '@angular/material/card';
-import { MatRadioModule } from '@angular/material';
-import {MatDialogModule} from '@angular/material/dialog';
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
 
@@ -24,27 +19,6 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { ProfileComponent } from './pages/user/profile/profile.component';
 import { PopupComponent } from './popup/popup.component';
 //club-components
-import { DashboardClubComponent } from './teams/club/platform_club/dashboard-club/dashboard-club.component';
-import { AccueilClubComponent } from './teams/club/platform_club/accueil-club/accueil-club.component';
-import { MembresComponent } from './teams/club/platform_club/membres/membres.component';
-import { ListeDemandesComponent } from './teams/club/platform_club/liste-demandes/liste-demandes.component';
-import { SigninComponent } from './teams/club/general/signin/signin.component';
-import { EnvoyerDemandeComponent } from './teams/club/general/envoyer-demande/envoyer-demande.component';
-import { ProfileMembreComponent } from './teams/club/platform_club/profile-membre/profile-membre.component';
-import { ResetPasswordComponent } from './teams/club/general/reset-password/reset-password.component';
-import { ClubsComponent } from './teams/club/general/clubs/clubs.component';
-import { SendRequestComponent } from './teams/club/general/send-request/send-request.component';
-import { CommentaireComponent } from './teams/club/platform_club/commentaire/commentaire.component';
-import { VotesComponent } from './teams/club/platform_club/votes/votes.component';
-import { TestaccueilComponent } from './teams/club/platform_club/testaccueil/testaccueil.component';
-import { SearchPipe } from './teams/club/platform_club/liste-demandes/search.pipe';
-import { HeaderComponent } from './teams/club/general/header/header.component';
-import { ListParticipesComponent } from './teams/club/platform_club/list-participes/list-participes.component';
-import { ListeParticipationComponent } from './teams/club/platform_club/liste-participation/liste-participation.component';
-import { CalendrierComponent } from './teams/club/platform_club/calendrier/calendrier.component';
-import { ScheduleComponent } from './teams/club/general/schedule/schedule.component';
-import { DetailsEventComponent } from './teams/club/platform_club/details-event/details-event.component';
-import { DetailEventAccueilComponent } from './teams/club/general/detail-event-accueil/detail-event-accueil.component';
 
 registerLocaleData(localeFr, 'fr');
 //administration-components
@@ -62,27 +36,7 @@ registerLocaleData(localeFr, 'fr');
     ProfileComponent,
     PopupComponent,
     //club-components
-    DashboardClubComponent,
-    AccueilClubComponent,
-    MembresComponent,
-    ListeDemandesComponent,
-    SigninComponent,
-    EnvoyerDemandeComponent,
-    ProfileMembreComponent,
-    ResetPasswordComponent,
-    ClubsComponent,
-    SendRequestComponent,
-    CommentaireComponent,
-    VotesComponent,
-    TestaccueilComponent,
-    SearchPipe,
-    CalendrierComponent,
-    HeaderComponent,
-    ListParticipesComponent,
-    ListeParticipationComponent,
-    ScheduleComponent,
-    DetailsEventComponent,
-    DetailEventAccueilComponent,
+
     //administration-components
     //communication-components
     //scolarite-components
@@ -97,14 +51,7 @@ registerLocaleData(localeFr, 'fr');
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    MatCardModule,
-    MatRadioModule,
-    MatDialogModule,
-    NgbModalModule,
-    CalendarModule.forRoot({
-      provide: DateAdapter,
-      useFactory: adapterFactory,
-    }),
+
 
   ],
   entryComponents: [PopupComponent],
